a90035e761e8597a243b60b257c187c5364deff5
Joanna Buchała <joanna.b2@op.pl>
2018-05-24 19:42:01 +0200

    environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,37 @@
+// production - time: 3546ms, size: 117 KiB 
+// development - time: 15892ms, size: 627 KiB
+
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
+module.exports = (env) => {
+    const environment = env;
+    return {
+        mode: environment,
+        entry: './src/index.js',
         output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader"
-            },
-            {
-            	test: /\.css$/,
-            	use: [
-            		{ loader: 'style-loader'},
-            		{
-            			loader: 'css-loader',
-            			options: {
-            				modules: true
-            			}
-            		}
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.' + environment + '.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader"
+                },
+                {
+                	test: /\.css$/,
+                	use: [
+                		{ loader: 'style-loader'},
+                		{
+                			loader: 'css-loader',
+                			options: {
+                				modules: true
+                			}
+                		}
 
-            	]
-            }
-        ]
-    }
+                	]
+                }
+            ]
+        }
+    };
 };
